ba6dcc40da9fe8c6faabe708ac4e47565b6ccca8
Barry Becker <barrybecker4@gmail.com>
2020-11-09 06:06:12 -0800

    Use H2 database to store transactions
    The persistent H2 instance works, but still need to log transactions.
    Add utility to encrypt passwords.

---
+++ bxbot-rest-api/src/test/java/com/gazbert/bxbot/rest/api/security/authentication/TestBCrypt.java
@@ -0,0 +1,19 @@
+package com.gazbert.bxbot.rest.api.security.authentication;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestBCrypt {
+
+  private static final BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder();
+
+  /** Generate an encrypted pw */
+  public static void main(String[] args) {
+
+    encrypt("some string to encrypt");
+  }
+
+  private static void encrypt(String s) {
+    System.out.println(s + " -> '" + ENCODER.encode(s) +  "'");
+  }
+
+}
